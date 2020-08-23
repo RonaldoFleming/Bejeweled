3dba5ea44f1778e2843d6b4662ecacd070bf6e27
RonaldoFleming <christof666@msn.com>
2020-08-23 19:21:55 -0400

    Implemented the main menu, Score and some minor bug fixes.

---
+++ Assets/_Scripts/Controllers/ScoreBarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBarController : MonoBehaviour
+{
+    [SerializeField] private Text _scoreText = default;
+    
+    void Start()
+    {
+        _scoreText = GetComponent<Text>();
+        _scoreText.text = "0";
+        EventBus.OnScoreUpdated += OnScoreUpdated;
+    }
+
+    private void OnDestroy()
+    {
+        EventBus.OnScoreUpdated -= OnScoreUpdated;
+    }
+
+    private void OnScoreUpdated(object sender, System.EventArgs e)
+    {
+        _scoreText.text = GameManager.Instance.Score.ToString();
+    }
+}
---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    public int Score { get; private set; }
+
+    void Start()
+    {
+        EventBus.OnGameStarted += OnGameStarted;
+        EventBus.OnNodeDestroyed += OnNodeDestroyed;
+    }
+
+    private void OnDestroy()
+    {
+        EventBus.OnGameStarted -= OnGameStarted;
+        EventBus.OnNodeDestroyed -= OnNodeDestroyed;
+    }
+
+    private void OnGameStarted(object sender, System.EventArgs e)
+    {
+        Score = 0;
+    }
+
+    private void OnNodeDestroyed(object sender, System.EventArgs e)
+    {
+        Score++;
+        EventBus.RaiseScoreUpdated(this);
+    }
+}
